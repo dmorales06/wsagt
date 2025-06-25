f728937bf5ac238194aacf20d37356f59b5534ce
dmorales06 <diegomorales.dm53@gmail.com>
2025-06-24 21:14:42 -0600

    Departamentos(Areas) y Roles

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,5 @@
-import {poolAGT} from '../conectionBD.js';
-import {generarToken} from '../auth.js';
+import {poolAGT} from '../DBS/conectionBD_mysql.js';
+import {generarToken} from '../authenticacion/authJWT.js';
 
 const controller = {};
 
@@ -11,11 +11,12 @@ controller.generarToken = async (req, res) => {
         const [usuarios] = await poolAGT.query('select * from usuario where status=?', ['A']);
         const usuarioObtenido = usuarios.find(c => c.id_usuario === id_usuario);
 
-        console.log(id_usuario, usuario, contrasena);
-
         if (usuarioObtenido.id_usuario == id_usuario && usuarioObtenido.usuario == usuario && usuarioObtenido.contrasena == contrasena) {
+
             const token = generarToken(usuarioObtenido);
             res.json({token});
+        }else{
+            res.status(404).send("contraseña o usuario incorrecto");
         }
     }catch(err){
         res.status(404).send("El usuario no existe o esta inhabilitado.");
@@ -24,7 +25,7 @@ controller.generarToken = async (req, res) => {
 
 }
 
-//usuarios generales
+//obtener usuarios generales
 controller.usuarios = async (req, res) => {
     const [usuarios] = await poolAGT.query('select * from usuario');
     res.json(usuarios);
@@ -84,7 +85,7 @@ controller.delusuario = async (req, res) => {
     else {
         const [result] = await poolAGT.query('delete from usuario where id_usuario = ?', [usuario.id_usuario]);
         if (result.affectedRows === 1) {
-            res.json({msg: 'Usuario eliminado exitosamente'});
+            res.status(201).json({msg: 'Usuario elimiado exitosamente'});
         }
     }
 }
@@ -106,10 +107,51 @@ controller.updusuario = async (req, res) => {
     else {
         const [result] = await poolAGT.query('update usuario set ? where id_usuario = ?', [updateData, usuario.id_usuario]);
         if (result.affectedRows === 1) {
-            res.json({msg: 'Usuario actualizado exitosamente'});
+            res.status(201).json({msg: 'Usuario actualizado exitosamente'});
         }
     }
 }
 
+//----------------------------------------------------------------------------
+//Obtener areas de empresa
+controller.area = async (req, res) => {
+    const [departametno] = await poolAGT.query('select * from departamento');
+    res.json(departametno);
+}
+
+//Obtener area de empresa por ID
+controller.areaID = async (req, res) => {
+    const [areas] = await poolAGT.query('select * from departamento');
+    const area = areas.find(c => c.id_departamento === parseInt(req.params.id));
+    if (!area) return res.status(404).json({msg: 'Departamento no encontrado'});
+    else res.json(area);
+}
+
+//----------------------------------------------------------------------------
+//Roles
+controller.roles = async (req, res) => {
+    const [roles] = await poolAGT.query('select * from rol');
+    res.json(roles);
+}
+
+//Actualizar permisos de Rol
+controller.updrol = async (req, res) => {
+    const [roles] = await poolAGT.query('select * from rol');
+    const rol = roles.find(c => c.nombre === req.params.rol);
+
+    const updateData = {
+        endpoint:req.body.endpoint,
+    }
+    console.log(rol);
+
+    if (!rol) return res.status(404).json({msg: 'Rol no encontrado'});
+
+    else {
+        const [result] = await poolAGT.query('update rol set ? where id_rol = ?', [updateData, rol.id_rol]);
+        if (result.affectedRows === 1) {
+            res.status(201).json({msg: 'Permisos de rol actualizados exitosamente'});
+        }
+    }
+}
 
 export default controller;
--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import customerController from '../controllers/customerController.js';
-import {verificarToken} from "../middleware.js";
+import {verificarToken} from "../Middleware/middlewareJWT.js";
 
 const router = express.Router();
 
@@ -9,10 +9,18 @@ router.post('/api/gentoken',customerController.generarToken);
 
 //Gestion de usuarios
 router.get('/api/users',verificarToken,customerController.usuarios);
-router.get('/api/users/:id',customerController.usuarioID);
-router.post('/api/users',customerController.crtusuario);
-router.put('/api/users/:id',customerController.updusuario);
-router.delete('/api/users/:id',customerController.delusuario);
+router.get('/api/users/:id',verificarToken,customerController.usuarioID);
+router.post('/api/users',verificarToken,customerController.crtusuario);
+router.put('/api/users/:id',verificarToken,customerController.updusuario);
+router.delete('/api/users/:id',verificarToken,customerController.delusuario);
+
+//Areas
+router.get('/api/area',verificarToken,customerController.area);
+router.get('/api/area/:id',verificarToken,customerController.areaID);
+
+//Roles
+router.get('/api/roles',customerController.roles);
+router.post('/api/rol/:rol',customerController.updrol);
 
 
 
